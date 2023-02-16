704c27ee76f720013a76cdf4bda4db7a3507910a
yolimarisol <yolamarisol@hotmail.com>
2023-02-16 09:35:58 -0600

    feat particles

---
+++ js/app.js
@@ -0,0 +1,112 @@
+particlesJS(
+    {
+        "particles": {
+          "number": {
+            "value": 120,
+            "density": {
+              "enable": true,
+              "value_area": 800
+            }
+          },
+          "color": {
+            "value": "#4c6984"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 2,
+              "color": "#2d317a"
+            },
+            "polygon": {
+              "nb_sides": 4
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 2000,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.561194221302933,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 8.017060304327615,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 144.30708547789706,
+            "color": "#4c6984",
+            "opacity": 0.43292125643369117,
+            "width": 7.8567190982410615
+          },
+          "move": {
+            "enable": true,
+            "speed": 8.017060304327615,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 1362.9002517356944,
+              "rotateY": 1042.21783956259
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "bubble"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      }
+);
